57df6144485848b0f6c222b838a3aaafc4056c04
Marsden Mathews Odhiambo <marsdenlwero@gmail.com>
2024-08-27 17:22:00 +0300

    Completes Lab exercise

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <>
+      <h1>Hi, I'm Marsden</h1>
+      <img alt="My profile" src="https://via.placeholder.com/350" />
+      <h2>About Me</h2>
+      <p>This is my bio</p>
+
+      <div>
+        <a href="https://github.com">GitHub</a>
+        <a href="https://linkedin.com">LinkedIn</a>
+      </div>
+    </>
+
+  )
 }
 
 export default App;
